4447109112735f2bdb05c1154808ba017b710041
Nikola Pavlović <nikolap994@gmail.com>
2022-10-04 18:16:43 +0200

    Add Basic setup for users

--- models/User.js
+++ models/User.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-});
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, "is invalid"],
+      index: true,
+    },
+    firstName: String,
+    lastName: String,
+    password: String,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
