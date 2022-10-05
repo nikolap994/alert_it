300d9b7d38e3f5455717e3e0f009343ac598b60e
Nikola Pavlović <nikolap994@gmail.com>
2022-10-05 19:45:13 +0200

    Add monitor GET request

--- pages/api/monitors.js
+++ pages/api/monitors.js
@@ -9,6 +9,13 @@ export default async function handler(req, res) {
   switch (method) {
     case "GET":
       try {
+        const userId = req.query.userId;
+        if (userId) {
+          const monitors = await Monitor.find({ownder: userId});
+          res.status(200).json({ success: true, data: monitors });
+        } else {
+          res.status(400).json({ success: false });
+        }
       } catch (error) {
         res.status(400).json({ success: false });
       }
