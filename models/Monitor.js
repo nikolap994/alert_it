63956e9f500df225e7d77b83e57c719dbb012daf
Nikola Pavlović <nikolap994@gmail.com>
2022-10-05 19:44:59 +0200

    Update Monitor Schema

--- models/Monitor.js
+++ models/Monitor.js
@@ -2,24 +2,23 @@ import mongoose from "mongoose";
 
 const MonitorSchema = new mongoose.Schema(
   {
+    /**
+     * monitorType: HTTP(s),  TCP Port, Ping
+     * url: Website url
+     * heartbeat: how often we check
+     * retries: how many times we recheck after failure
+     * acceptedStatusCodes: allowed status codes, for example 200-299
+     */
     name: String,
     url: String,
     heartbeat: Number,
     retries: Number,
     acceptedStatusCodes: String,
     monitorType: String,
-    owner: String, // this should be User ID, not a string
-
+    owner: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.models.Monitor || mongoose.model("Monitor", MonitorSchema);
-
-/**
- * monitorType: HTTP(s),  TCP Port, Ping
- * url: Website url
- * heartbeat: how often we check
- * retries: how many times we recheck after failure
- * acceptedStatusCodes: allowed status codes, for example 200-299
- */
+module.exports =
+  mongoose.models.Monitor || mongoose.model("Monitor", MonitorSchema);
