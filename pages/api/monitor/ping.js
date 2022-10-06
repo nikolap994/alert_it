bb4112682847954527703f7fd72bb049bf2eecf1
Nikola Pavlović <nikolap994@gmail.com>
2022-10-06 18:09:34 +0200

    Add Ping checker

---
+++ pages/api/monitor/ping.js
@@ -0,0 +1,21 @@
+const ping = require("ping");
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const url = req.query.url;
+        ping.promise.probe(url).then(function (res) {
+          res.status(200).json({ alive: res.alive });
+        });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
