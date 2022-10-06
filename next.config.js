9623cd196a978e3032a88dce3aae7b193a63ce82
Nikola Pavlović <nikolap994@gmail.com>
2022-10-06 18:45:06 +0200

    Disable cronjob spam

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-require("./lib/cron");
+// require("./lib/cron"); //Temproray disabled until more of the frontned is done.
 
 const nextConfig = {
   reactStrictMode: true,
