f612be8e4b2b39ab95a20b4504bd80c9cacfbe6e
nikolap994 <nikolap994@gmail.com>
2022-10-10 23:19:44 +0200

    Add tailwind animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,25 @@ module.exports = {
         "catamaran-medium": ["Catamaran-Medium"],
         "catamaran-bold": ["Catamaran-Bold"],
       },
+      animation: {
+        "fade-in": "fade-in linear 0.25s forwards",
+        "fade-out": "fade-out linear 1s forwards",
+        "slide-down": "slide-down ease-out 1s"
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        },
+        "fade-out": {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 }
+        },
+        "slide-down": {
+          "0%": { transform: "translateY(-70px)" },
+          "100%": { transform: "translateY(0px)" }
+        }
+      }
     },
   },
   plugins: [],
