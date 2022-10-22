d08554481cdad227fa5f29a16ac0d9495f07fd25
Nikola Pavlović <nikolap994@gmail.com>
2022-10-22 23:55:54 +0200

    Add boilerplate for signin form

--- pages/signin.js
+++ pages/signin.js
@@ -1,13 +1,35 @@
-import SignInCTA from "../src/components/signin-cta";
-import SignInForm from "../src/components/sign-in-form";
+import { getCsrfToken, getSession } from "next-auth/react";
 
-export default function SignIn() {
+export default function SignIn({ csrfToken }) {
 	return (
-		<section className="bg-white dark:bg-gray-900">
-			<div className="flex justify-center h-screen">
-				<SignInCTA />
-				<SignInForm />
-			</div>
-		</section>
+		<form method="post" action="/api/auth/callback/credentials">
+			<input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+			<label>
+				Email
+				<input name="email" type="email" />
+			</label>
+			<label>
+				Password
+				<input name="password" type="password" />
+			</label>
+			<button type="submit">Sign in</button>
+		</form>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const { req } = context;
+	const session = await getSession({ req });
+
+	if (session) {
+		return {
+			redirect: { destination: "/" },
+		};
+	}
+
+	return {
+		props: {
+			csrfToken: await getCsrfToken(context),
+		},
+	};
 }
