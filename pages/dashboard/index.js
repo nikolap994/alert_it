292e0b78e7f86b24c0e4eccfc253daef4281a4c3
Nikola Pavlović <nikolap994@gmail.com>
2022-10-24 19:27:52 +0200

    Rename admin to dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,17 @@
-function Dashboard() {
-	return (
-		<div>Dashboard</div>
-	);
+import { getSession } from "next-auth/react";
+
+function Dashboard({ dashboardData }) {
+	return <h1>Dashboard Page - {dashboardData}</h1>;
 }
 
 export default Dashboard;
+
+export async function getServerSideProps(context) {
+	const session = await getSession(context);
+	console.log(session);
+	return {
+		props: {
+			dashboardData: session ? "Logged in" : "Not Logged in",
+		},
+	};
+}
