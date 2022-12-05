5ef57c8757770bb387f7e7cba87c0797c87499bf
Cehicmm <cehicmarija@gmail.com>
2022-12-06 00:00:49 +0100

    Update 404 page styles

--- pages/404.js
+++ pages/404.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Custom404() {
 	return (
 		<section className="bg-gray-900 text-white text-center">
-			<div className="h-screen pt-[10%]">
+			<div className="h-screen pt-[10%] max-w-7xl mx-auto">
 				<h1 className="text-9xl">
 					4<span className="text-indigo-200">o</span>4
 				</h1>
 				<div>
 					<h2 className="text-3xl pt-12">Oooppsie!</h2>
-					<p className="pt-8">
+					<p className="pt-8 max-w-[500px] mx-auto">
 						The page you're trying to reach has been moved or doesn't exist.
 						Restarting from <Link href="/">Home</Link> page and you will
 						undoubtedly fix the issue.
