9dc8fb887c4f219d81990c09fd82c7ff0a1c517f
Cehicmm <cehicmarija@gmail.com>
2022-12-05 23:55:19 +0100

    Fix formatting in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,39 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        "catamaran-light": ["Catamaran-Light"],
-        "catamaran-regular": ["Catamaran-Regular"],
-        "catamaran-medium": ["Catamaran-Medium"],
-        "catamaran-bold": ["Catamaran-Bold"],
-      },
-      animation: {
-        "fade-in": "fade-in linear 0.25s forwards",
-        "fade-out": "fade-out linear 1s forwards",
-        "slide-down": "slide-down ease-out 1s"
-      },
-      keyframes: {
-        "fade-in": {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
-        },
-        "fade-out": {
-          "0%": { opacity: 1 },
-          "100%": { opacity: 0 }
-        },
-        "slide-down": {
-          "0%": { transform: "translateY(-70px)" },
-          "100%": { transform: "translateY(0px)" }
-        }
-      }
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				"catamaran-light": ["Catamaran-Light"],
+				"catamaran-regular": ["Catamaran-Regular"],
+				"catamaran-medium": ["Catamaran-Medium"],
+				"catamaran-bold": ["Catamaran-Bold"],
+			},
+			animation: {
+				"fade-in": "fade-in linear 0.25s forwards",
+				"fade-out": "fade-out linear 1s forwards",
+				"slide-down": "slide-down ease-out 1s",
+			},
+			keyframes: {
+				"fade-in": {
+					"0%": { opacity: 0 },
+					"100%": { opacity: 1 },
+				},
+				"fade-out": {
+					"0%": { opacity: 1 },
+					"100%": { opacity: 0 },
+				},
+				"slide-down": {
+					"0%": { transform: "translateY(-70px)" },
+					"100%": { transform: "translateY(0px)" },
+				},
+			},
+		},
+	},
+	plugins: [],
+};
