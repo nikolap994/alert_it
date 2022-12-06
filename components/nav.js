0560595463571247b9207f9bdb8d7932f3308399
Nikola Pavlović <nikolap994@gmail.com>
2022-12-06 17:29:44 +0100

    Update navbar for logged in users

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,8 @@
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
+import Image from "next/image";
+import { md5 } from "request/lib/helpers";
 
 function Navigation() {
 	const { data: session } = useSession();
@@ -8,21 +10,36 @@ function Navigation() {
 
 	return (
 		<nav>
-			<ul className="flex gap-4 text-white">
-				<li className="">
-					<Link href="/"> Home </Link>
-				</li>
-				<li className="">
-					<Link href="/pricing"> Pricing </Link>
-				</li>
-				<li className="">
-					<Link href="/contact"> Contact </Link>
-				</li>
-				<li className="">
-					<Link href="/signin">
-						{session ? (
-							<button onClick={() => signOut()}>Log out</button>
-						) : (
+			{session ? (
+				<ul className="flex gap-4 text-white">
+					<li className="">
+						<Link href="/dashboard"> Dashboard </Link>
+					</li>
+					<li className="">
+						<Link href="/dashboard/user">
+							{"Edit profile"}
+							<Image
+								alt="profile-pic"
+								src={
+									"https://www.gravatar.com/avatar/" + md5(session.user.email)
+								}
+								width={50}
+								height={50}
+								className="w-8 h-8 rounded-full"
+							></Image>
+						</Link>
+					</li>
+					<li className="">
+						<button onClick={() => signOut()}>Log out</button>
+					</li>
+				</ul>
+			) : (
+				<ul className="flex gap-4 text-white">
+					<li className="">
+						<Link href="/"> Home </Link>
+					</li>
+					<li className="">
+						<Link href="/signin">
 							<button
 								onClick={() => {
 									router.push("/api/auth/signin");
@@ -30,10 +47,10 @@ function Navigation() {
 							>
 								Sign in
 							</button>
-						)}
-					</Link>
-				</li>
-			</ul>
+						</Link>
+					</li>
+				</ul>
+			)}
 		</nav>
 	);
 }
