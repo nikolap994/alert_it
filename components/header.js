3553fa2b59b58108fa813fe3ba087cb0e6db6522
Cehicmm <cehicmarija@gmail.com>
2022-12-06 20:42:34 +0100

    Clean up header

--- components/header.js
+++ components/header.js
@@ -1,25 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
-
 import Navigation from "./nav";
-
-import logoImg from "../public/images/logo.png";
 
 function Header() {
 	return (
 		<header className="sticky z-50 w-full bg-slate-800">
-			<div className="max-w-7xl mx-auto px-4 md:px-6 flex justify-between items-center">
-				<Link className="flex" href="/">
-					<Image
-						className="h-full"
-						src={logoImg}
-						alt="Logo image"
-						width={100}
-						height={75}
-					/>
-				</Link>
-				<Navigation />
-			</div>
+			<Navigation />
 		</header>
 	);
 }
