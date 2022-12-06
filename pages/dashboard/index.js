63fad9b8afcb2c22c65193dead2bbe6d523248bf
Nikola Pavlović <nikolap994@gmail.com>
2022-12-06 17:29:58 +0100

    Update button styles on dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -6,36 +6,42 @@ export default function Dashboard(props) {
 	return (
 		<main className="mt-10 max-w-7xl mx-auto px-4 md:px-6">
 			<h1>Dashboard Page</h1>
-			<div>Logged in as: {props.email}</div>
-			<div>ID: {props.id}</div>
-			<div>
-				{props.firstName} {props.lastName}
-			</div>
 
+			<Link
+				className="text-white bg-blue-700 rounded"
+				href="/dashboard/monitor/create"
+			>
+				Create new Monitor
+			</Link>
 			<div>
 				<br />
 				Monitors:
 				{props.monitors.length > 0 &&
 					props.monitors.map(monitor => (
-						<div key={monitor.id}>
-							<p key={monitor.id + "_ID"}>Monitor ID: {monitor._id}</p>
-							<p key={monitor.id + "_Name"}>Name: {monitor.name}</p>
-							<p key={monitor.id + "_URL"}>URL: {monitor.url}</p>
-							<p key={monitor.id + "_heartbeat"}>
+						<div key={monitor._id}>
+							<p key={monitor._id + "_ID"}>Monitor ID: {monitor._id}</p>
+							<p key={monitor._id + "_Name"}>Name: {monitor.name}</p>
+							<p key={monitor._id + "_URL"}>URL: {monitor.url}</p>
+							<p key={monitor._id + "_heartbeat"}>
 								heartbeat: {monitor.heartbeat}
 							</p>
-							<p key={monitor.id + "_monitorType"}>
+							<p key={monitor._id + "_monitorType"}>
 								monitorType: {monitor.monitorType}
 							</p>
-							<p key={monitor.id + "_retries"}>retries: {monitor.retries}</p>
+							<p key={monitor._id + "_retries"}>retries: {monitor.retries}</p>
 							<Image
-								key={monitor.id + "_IMAGE"}
+								key={monitor._id + "_IMAGE"}
 								width={300}
 								height={300}
 								src={"data:image/gif;base64," + monitor.image}
 								alt="monitor"
 							/>
-							<Link href="/dashboard/monitor/edit">Edit</Link>
+							<Link
+								className="text-white bg-blue-700 rounded"
+								href={"/dashboard/monitor/edit/" + monitor._id}
+							>
+								Edit
+							</Link>
 							<br />
 						</div>
 					))}
@@ -94,8 +100,4 @@ export async function getServerSideProps(context) {
 		response.props.monitors = monitors;
 		return response;
 	}
-
-	return {
-		props: {},
-	};
 }
