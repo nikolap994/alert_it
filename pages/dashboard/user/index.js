fca53995673e76838fc859b2c5253b883ec306c8
Nikola Pavlović <nikolap994@gmail.com>
2022-12-09 18:03:21 +0100

    Update user form

--- pages/dashboard/user/index.js
+++ pages/dashboard/user/index.js
@@ -16,27 +16,61 @@ function User(props) {
 	};
 
 	return (
-		<main className="mt-10 max-w-7xl mx-auto px-4 md:px-6">
-			<div>User Page</div>
-			<form method="POST" action="" onSubmit={submitForm}>
-				<div>
-					<label>Firstname</label>
-					<input type="text" name="firstName" defaultValue={props.firstName} />
+		<div className="mt-10 max-w-7xl mx-auto px-4 md:px-6">
+			<div className="block mb-2 dark:text-white">User Page</div>
+			<form
+				className="w-full max-w-lg"
+				method="POST"
+				action=""
+				onSubmit={submitForm}
+			>
+				<div className="mb-6">
+					<label htmlFor="firstName" className="block mb-2 dark:text-white">
+						Firstname
+					</label>
+					<input
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						type="text"
+						name="firstName"
+						id="firstName"
+						defaultValue={props.firstName}
+					/>
 				</div>
 
-				<div>
-					<label>Lastname</label>
-					<input type="text" name="lastName" defaultValue={props.lastName} />
+				<div className="mb-6">
+					<label htmlFor="lastName" className="block mb-2 dark:text-white">
+						Lastname
+					</label>
+					<input
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						type="text"
+						id="lastName"
+						name="lastName"
+						defaultValue={props.lastName}
+					/>
 				</div>
 
-				<div>
-					<label>Email</label>
-					<input type="text" name="email" defaultValue={props.email} />
+				<div className="mb-6">
+					<label htmlFor="email" className="block mb-2 dark:text-white">
+						Email
+					</label>
+					<input
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						type="text"
+						name="email"
+						id="email"
+						defaultValue={props.email}
+					/>
 				</div>
 
-				<button type="submit">Save</button>
+				<button
+					className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					type="submit"
+				>
+					Save
+				</button>
 			</form>
-		</main>
+		</div>
 	);
 }
 
