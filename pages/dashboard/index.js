313556e4434c9538d035843e8d83564cbb4db966
Nikola Pavlović <nikolap994@gmail.com>
2022-12-09 18:44:54 +0100

    Update layout styles

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -5,23 +5,29 @@ import Image from "next/image";
 export default function Dashboard(props) {
 	return (
 		<div className="max-w-7xl mx-auto px-4 md:px-6 text-white">
-			<div className="pt-10 flex justify-between items-center max-w-xl">
+			<div className="pt-10 flex justify-between items-center max-w">
 				<h1 className="text-5xl my-16">Dashboard Page</h1>
 
 				<Link
-					className="text-white bg-blue-700 rounded h-16 p-6 text-center"
+					className="text-white bg-blue-700 rounded h-16 p-6 text-center float-right"
 					href="/dashboard/monitor/create"
 				>
 					Create new Monitor
 				</Link>
 			</div>
-			<div>
-				<h2 className="text-3xl mb-10">Monitors:</h2>
-
+			<div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
 				{props.monitors.length > 0 &&
 					props.monitors.map((monitor) => (
-						<div key={monitor._id} className="flex mb-12">
-							<div className="w-1/2 flex flex-col gap-4">
+						<div key={monitor._id} className="max-w-sm rounded overflow-hidden shadow-lg shadow-blue-500/50">
+							<Image
+								className="w-full p-2 rounded border-2 border-blue-500"
+								key={monitor._id + "_IMAGE"}
+								width={300}
+								height={300}
+								src={"data:image/gif;base64," + monitor.image}
+								alt="monitor"
+							/>
+							<div className="px-6 py-4">
 								<p key={monitor._id + "_Name"}>Name: {monitor.name}</p>
 								<p key={monitor._id + "_URL"}>URL: {monitor.url}</p>
 								<p key={monitor._id + "_heartbeat"}>
@@ -31,21 +37,15 @@ export default function Dashboard(props) {
 									monitorType: {monitor.monitorType}
 								</p>
 								<p key={monitor._id + "_retries"}>retries: {monitor.retries}</p>
+								<p className="pt-5">
 								<Link
-									className="text-white bg-blue-700 rounded w-24 text-center p-2"
+									className="text-white bg-blue-700 rounded text-center w-full inline-block"
 									href={"/dashboard/monitor/edit/" + monitor._id}
 								>
 									Edit
 								</Link>
+								</p>
 							</div>
-							<Image
-								className="w-1/2"
-								key={monitor._id + "_IMAGE"}
-								width={300}
-								height={300}
-								src={"data:image/gif;base64," + monitor.image}
-								alt="monitor"
-							/>
 						</div>
 					))}
 			</div>
