9a7fdb83ec3fdc6db807707045d4c7521964f300
Nikola Pavlović <nikolap994@gmail.com>
2022-12-09 19:03:54 +0100

    Add min and max retries

--- pages/dashboard/monitor/create.js
+++ pages/dashboard/monitor/create.js
@@ -74,6 +74,8 @@ export default function CreateMonitor() {
 					</label>
 					<input
 						type="number"
+						min={1}
+						max={5}
 						required
 						id="retries"
 						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
--- pages/dashboard/monitor/edit/[page].js
+++ pages/dashboard/monitor/edit/[page].js
@@ -80,6 +80,8 @@ function EditMonitor(props) {
 					<input
 						type="number"
 						id="retries"
+						min={1}
+						max={5}
 						defaultValue={props.retries}
 						required
 						className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
