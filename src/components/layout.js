90e73a988b405a3aba80432e0d826174544f17ba
Nikola Pavlović <nikolap994@gmail.com>
2022-12-09 17:55:33 +0100

    Fix layout color

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 export default function Layout({ children }) {
 	return (
-		<div className="flex h-full flex-col bg-slate-900">
-			<main>{children}</main>
+		<div className="flex h-screen flex-col bg-slate-900">
+			<main className="bg-slate-900">{children}</main>
 		</div>
 	);
 }
