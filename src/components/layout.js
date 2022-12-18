cab81a3efc66ae566b8beb4490cc139a954a7b53
Cehicmm <cehicmarija@gmail.com>
2022-12-18 18:31:33 +0100

    Remove height, bg color from Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,3 @@
 export default function Layout({ children }) {
-	return (
-		<div className="flex h-screen flex-col bg-slate-900">
-			<main className="bg-slate-900">{children}</main>
-		</div>
-	);
+	return <main className="bg-slate-900">{children}</main>;
 }
