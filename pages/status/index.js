ac46df78d6f01627989107ad5585a8556e1bff12
bairamov <ellezbayramov2004@gmail.com>
2023-01-02 23:44:22 +0300

    Typo fix

--- pages/status/index.js
+++ pages/status/index.js
@@ -14,7 +14,7 @@ function SearchMonitor() {
     {
       router.push(`/status/${searchId}`);
     }else{ 
-      alert("Please enter invalid ID");
+      alert("Please enter valid ID");
     }
 
   }
@@ -28,7 +28,7 @@ function SearchMonitor() {
     <div className = "h-screen flex justify-center">
       <form onSubmit = {handleSubmit} className = "flex h-10 space-x-2 mt-20">
           <input
-            class="placeholder:italic placeholder:text-slate-500 text-base block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Monitor ID" type="text" name="searchInput"
+            className="placeholder:italic placeholder:text-slate-500 text-base block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Monitor ID" type="text" name="searchInput"
             required
           />
           <input type="submit" value="Search" className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" />
