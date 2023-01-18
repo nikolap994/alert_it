5aa0f8ff557ee3d6a24a0ba80656ef98ee0cc1cc
Cehicmm <cehicmarija@gmail.com>
2023-01-18 21:13:09 +0100

    Update Alert.js codestyle

--- components/Alert.js
+++ components/Alert.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Alert(props) {
+	const capital = (text) => {
+		const alert = text.toLowerCase();
+		return alert.charAt(0).toUpperCase() + alert.slice(1);
+	};
 
-  const capital = (text) => {
-    const alert = text.toLowerCase();
-    return alert.charAt(0).toUpperCase() + alert.slice(1);
-  }
-
-  return (
-    <div>
-      {props.alert && <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-        <strong class="font-bold">{capital(props.alert.msg)} </strong>
-      </div>}
-    </div>
-
-  )
+	return (
+		<div>
+			{props.alert && (
+				<div
+					className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+					role="alert"
+				>
+					<strong className="font-bold">{capital(props.alert.msg)} </strong>
+				</div>
+			)}
+		</div>
+	);
 }
 
-export default Alert
+export default Alert;
