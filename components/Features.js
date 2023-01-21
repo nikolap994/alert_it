0aee641611139aaa85b68d1071fb797dde16eb9e
Cehicmm <cehicmarija@gmail.com>
2023-01-21 23:22:09 +0100

    Update Homepage "Features" components styles

--- components/Features.js
+++ components/Features.js
@@ -6,25 +6,25 @@ function Features() {
 		<section className="bg-gray-900 border border-blue-700">
 			<div className="flex flex-wrap max-w-6xl mx-auto border border-white">
 				<div className="relative w-full lg:w-1/4 py-10 px-8 text-center border-b lg:border-b-0 lg:border-r border-white">
-					<AiOutlineFall className="hidden lg:block lg:absolute fill-white w-12 h-12 -right-6" />
+					<AiOutlineFall className="absolute fill-white w-12 h-12 left-1/2 lg:left-auto lg:-right-6 -bottom-6 lg:bottom-auto" />
 					<h3 className="text-lg text-white font-semibold uppercase">
 						Monitoring uptime for HTTP(s) / TCP
 					</h3>
 				</div>
 				<div className="relative w-full lg:w-1/4 py-10 px-8 text-center border-b lg:border-b-0 lg:border-r border-white">
-					<AiOutlineFall className="hidden lg:block lg:absolute fill-white w-12 h-12 -right-6" />
+					<AiOutlineFall className="absolute fill-white w-12 h-12 left-1/2 lg:left-auto lg:-right-6 -bottom-6 lg:bottom-auto" />
 					<h3 className="text-lg text-white font-semibold uppercase">
 						Fancy, Reactive, Fast UI/UX.
 					</h3>
 				</div>
 				<div className="relative w-full lg:w-1/4 py-10 px-8 text-center border-b lg:border-b-0 lg:border-r border-white">
-					<AiOutlineFall className="hidden lg:block lg:absolute fill-white w-12 h-12 -right-6" />
+					<AiOutlineFall className="absolute fill-white w-12 h-12 left-1/2 lg:left-auto lg:-right-6 -bottom-6 lg:bottom-auto" />
 					<h3 className="text-lg text-white font-semibold uppercase">
 						Elaborate notification system
 					</h3>
 				</div>
 				<div className="relative w-full lg:w-1/4 py-10 px-8 text-center border-b lg:border-b-0 lg:border-r border-white">
-					<AiOutlineFall className="hidden lg:block lg:absolute fill-white w-12 h-12 -right-6" />
+					<AiOutlineFall className="absolute fill-white w-12 h-12 left-1/2 lg:left-auto lg:-right-6 -bottom-6 lg:bottom-auto" />
 					<h3 className="text-lg text-white font-semibold uppercase">
 						1 second interval
 					</h3>
