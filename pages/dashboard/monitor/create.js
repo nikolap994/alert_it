d32fe5f7cece8140add3866392cab7734f7f7323
Marija Ćehić <cehicmarija@gmail.com>
2023-01-22 22:43:44 +0100

    Update Create Monitor widths, padding

--- pages/dashboard/monitor/create.js
+++ pages/dashboard/monitor/create.js
@@ -49,61 +49,73 @@ export default function CreateMonitor(props) {
 	};
 
 	return (
-		<div className="py-16 max-w-7xl ml-12 px-4 md:px-6 text-white max-w-[60vw]">
+		<div className="md:w-3/4 lg:w-1/2 max-w-[700px] py-16 px-8 md:px-12 text-white mx-auto">
 			<Head>
 				<title>Create new Monitor</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
-			<div className="block mb-2 dark:text-white text-4xl mb-12">
-				Create Monitor
-			</div>
+			<h1 className="mb-2 text-4xl mb-12">Create Monitor</h1>
 
-			<form className="w-full max-w-lg" method="POST" onSubmit={submitForm}>
-				<div className="mb-6">
+			<form
+				className="w-full flex flex-col gap-5"
+				method="POST"
+				onSubmit={submitForm}
+			>
+				<div>
 					<label className="block mb-2 dark:text-white" htmlFor="name">
 						Name
 					</label>
 					<input
 						required
 						id="name"
-						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
 						type="text"
 						name="name"
 					/>
 				</div>
 
-				<div className="mb-6">
+				<div>
 					<label className="block mb-2 dark:text-white" htmlFor="url">
 						URL
 					</label>
 					<input
 						id="url"
 						required
-						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
 						type="url"
 						name="url"
 					/>
 				</div>
 
-				<div className="mb-6">
+				<div>
 					<label className="block mb-2 dark:text-white" htmlFor="heartbeat">
 						Heartbeat
 					</label>
 					<select
 						id="heartbeat"
 						required
-						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
 						name="heartbeat"
 					>
-						<option defaultValue="1">1</option>
-						<option defaultValue="5">5</option>
-						<option defaultValue="10">10</option>
-						<option defaultValue="30">30</option>
-						<option defaultValue="60">60</option>
+						<option className="bg-slate-800 text-white" defaultValue="1">
+							1
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="5">
+							5
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="10">
+							10
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="30">
+							30
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="60">
+							60
+						</option>
 					</select>
 				</div>
 
-				<div className="mb-6">
+				<div>
 					<label
 						className="block mb-2 dark:text-white"
 						htmlFor="acceptedStatusCodes"
@@ -113,65 +125,85 @@ export default function CreateMonitor(props) {
 					<select
 						required
 						id="acceptedStatusCodes"
-						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
 						name="acceptedStatusCodes"
 					>
-						<option>Choose acceptedStatusCodes</option>
-						<option defaultValue="any">Any</option>
-						<option defaultValue="0-199">0-199</option>
-						<option defaultValue="200-299">200-299</option>
-						<option defaultValue="200-299">300-399</option>
-						<option defaultValue="200-299">400-499</option>
-						<option defaultValue="200-299">500-599</option>
+						<option>Choose accepted status codes</option>
+						<option className="bg-slate-800 text-white" defaultValue="any">
+							Any
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="0-199">
+							0-199
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="200-299">
+							200-299
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="200-299">
+							300-399
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="200-299">
+							400-499
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="200-299">
+							500-599
+						</option>
 					</select>
 				</div>
 
-				<div className="mb-6">
+				<div>
 					<label className="block mb-2 dark:text-white" htmlFor="monitorType">
 						Monitor Type
 					</label>
 					<select
 						id="monitorType"
 						required
-						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
 						name="monitorType"
 					>
 						<option>Choose a monitor type</option>
-						<option defaultValue="https">https</option>
-						<option defaultValue="tcp">tcp</option>
+						<option className="bg-slate-800 text-white" defaultValue="https">
+							https
+						</option>
+						<option className="bg-slate-800 text-white" defaultValue="tcp">
+							tcp
+						</option>
 					</select>
 				</div>
 
-				<div className="mb-6">
+				<div>
 					<label className="block mb-2 dark:text-white" htmlFor="port">
 						Port
 					</label>
 					<input
 						id="port"
 						required
-						className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
 						type="number"
 						name="port"
 					/>
 				</div>
 
-				<div className="mb-6">
+				<div>
 					<label className="block mb-2 dark:text-white" htmlFor="enabled">
 						Enabled
 					</label>
 					<select
 						id="enabled"
 						required
-						className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700"
 						name="enabled"
 					>
-						<option defaultValue="true">true</option>
-						<option defaultValue="false">false</option>
+						<option className="bg-emerald-800 text-white" defaultValue="true">
+							true
+						</option>
+						<option className="bg-red-800 text-white" defaultValue="false">
+							false
+						</option>
 					</select>
 				</div>
 
 				<button
-					className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					className="w-1/2 mx-auto mt-6 text-white bg-blue-700 hover:ring-2 hover:outline-none hover:ring-blue-300 rounded-lg px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 					type="submit"
 				>
 					Save
