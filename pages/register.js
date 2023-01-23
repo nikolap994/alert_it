644c88369eaad359a94603995902403a8aead87c
Marija Ćehić <cehicmarija@gmail.com>
2023-01-23 19:28:43 +0100

    Update register bg image, styles

--- pages/register.js
+++ pages/register.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import Image from "next/legacy/image";
 import Link from "next/link";
 
-import loginBg from "../public/images/login-bg.jpg";
+import loginBg from "../public/images/landscape.png";
 import eyeIcon from "../public/images/icons/eye-open.png";
 import eyeHideIcon from "../public/images/icons/eye-close.png";
 import Router from "next/router";
@@ -70,8 +70,8 @@ export default function Register() {
 	};
 
 	return (
-		<section className="">
-			<div className="">
+		<section>
+			<div>
 				<div className="relative h-full">
 					<Image
 						className="object-cover "
@@ -80,7 +80,7 @@ export default function Register() {
 						priority={true}
 						quality={100}
 					/>
-					<div className="absolute top-10 lg:top-24 px-6 mx-auto w-full md:w-3/4 lg:left-32 xl:w-1/2 xl:left-[25%]">
+					<div className="absolute top-10 lg:top-24 px-6 mx-auto w-full md:w-3/4 lg:left-32 xl:w-1/2 xl:left-[25%] text-black">
 						<div>
 							<div className="text-center">
 								<h2 className="text-4xl font-bold text-center text-white">
@@ -89,46 +89,40 @@ export default function Register() {
 							</div>
 							<form
 								onSubmit={onSubmit}
-								className="flex flex-col gap-6 mt-8 text-lg"
+								className="flex flex-col gap-6 mt-8 text-xl text-white"
 								method="post"
 								action="/api/auth/callback/credentials"
 							>
 								<div>
-									<label
-										htmlFor="firstname"
-										className="flex flex-col block text-gray-200"
-									>
+									<label htmlFor="firstname" className="flex flex-col block">
 										First name
 									</label>
 									<input
 										type="text"
 										name="firstname"
 										placeholder="Enter your first name"
-										className="h-12 bg-transparent block w-full px-4 py-2 mt-2 text-gray-200 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+										className="h-12 bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
 									></input>
 								</div>
 
 								<div>
-									<label
-										htmlFor="lastname"
-										className="flex flex-col block text-gray-200"
-									>
+									<label htmlFor="lastname" className="flex flex-col block">
 										Last name
 									</label>
 									<input
 										type="text"
 										name="lastname"
 										placeholder="Enter your last name"
-										className="h-12 text-gray-200 bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+										className="h-12 text-black bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
 									></input>
 								</div>
 
 								<div>
-									<label htmlFor="email" className="block text-gray-200">
+									<label htmlFor="email" className="block">
 										Email
 										<input
 											placeholder="ex: john@gmail.com"
-											className="h-12 bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+											className="text-black h-12 bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
 											autoComplete="email"
 											name="email"
 											type="email"
@@ -139,7 +133,7 @@ export default function Register() {
 								<div>
 									<label
 										htmlFor="password"
-										className="relative flex flex-col block text-white"
+										className="relative flex flex-col block"
 									>
 										Password
 										<input
@@ -148,11 +142,11 @@ export default function Register() {
 											autoComplete="current-password"
 											name="password"
 											type="password"
-											className="h-12 block bg-transparent w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+											className="text-black h-12 block bg-transparent w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
 										/>
 										<button className="absolute bottom-0 right-4" type="button">
 											<Image
-												className="h-auto w-auto invert"
+												className="h-auto w-auto"
 												src={isEyeOpen ? eyeIcon : eyeHideIcon}
 												alt="Eye icon"
 												width={30}
@@ -166,7 +160,7 @@ export default function Register() {
 								<div>
 									<label
 										htmlFor="repeatpassword"
-										className="relative flex flex-col block text-white"
+										className="relative flex flex-col block"
 									>
 										Repeat Password
 										<input
@@ -174,12 +168,12 @@ export default function Register() {
 											autoComplete="current-password"
 											name="repeatpassword"
 											type="password"
-											className="h-12 bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+											className="text-black h-12 bg-transparent block w-full px-4 py-2 mt-2 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
 										/>
 									</label>
 								</div>
 								<button
-									className="mx-auto py-2 px-8 mx-auto border border-transparent rounded-md bg-white fade-out hover:bg-indigo-600 hover:text-white"
+									className="mx-auto py-2 px-8 mx-auto border border-transparent rounded-md fade-out bg-indigo-600 hover:bg-indigo-900 text-white"
 									type="submit"
 								>
 									Sign Up
