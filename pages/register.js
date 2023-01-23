68e07edf39dd744337826f002d190dbd1ceaa337
Marija Ćehić <cehicmarija@gmail.com>
2023-01-23 20:20:08 +0100

    Update Sign Up styles

--- pages/register.js
+++ pages/register.js
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import Image from "next/legacy/image";
 import Link from "next/link";
 
-import loginBg from "../public/images/landscape.png";
 import eyeIcon from "../public/images/icons/eye-open.png";
 import eyeHideIcon from "../public/images/icons/eye-close.png";
 import Router from "next/router";
@@ -71,126 +70,118 @@ export default function Register() {
 
 	return (
 		<section>
-			<div>
-				<div className="relative h-full">
-					<Image
-						className="object-cover "
-						src={loginBg}
-						alt="Log in image"
-						priority={true}
-						quality={100}
-					/>
-					<div className="absolute top-10 lg:top-24 px-6 mx-auto w-full md:w-3/4 lg:left-32 xl:w-1/2 xl:left-[25%] text-black">
+			<div className="bg-slate-900 py-16 max-w-[500px] mx-auto">
+				<div className="px-6 text-black">
+					<h2 className="text-4xl font-bold text-center text-white">Sign Up</h2>
+
+					<form
+						onSubmit={onSubmit}
+						className="flex flex-col gap-6 mt-8 text-white"
+						method="post"
+						action="/api/auth/callback/credentials"
+					>
 						<div>
-							<div className="text-center">
-								<h2 className="text-4xl font-bold text-center text-white">
-									Sign Up
-								</h2>
-							</div>
-							<form
-								onSubmit={onSubmit}
-								className="flex flex-col gap-6 mt-8 text-xl text-white"
-								method="post"
-								action="/api/auth/callback/credentials"
+							<label
+								htmlFor="firstname"
+								className="flex flex-col block text-xl lg:text-2xl"
 							>
-								<div>
-									<label htmlFor="firstname" className="flex flex-col block">
-										First name
-									</label>
-									<input
-										type="text"
-										name="firstname"
-										placeholder="Enter your first name"
-										className="h-12 bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-									></input>
-								</div>
-
-								<div>
-									<label htmlFor="lastname" className="flex flex-col block">
-										Last name
-									</label>
-									<input
-										type="text"
-										name="lastname"
-										placeholder="Enter your last name"
-										className="h-12 text-black bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-									></input>
-								</div>
-
-								<div>
-									<label htmlFor="email" className="block">
-										Email
-										<input
-											placeholder="ex: john@gmail.com"
-											className="text-black h-12 bg-transparent block w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-											autoComplete="email"
-											name="email"
-											type="email"
-										/>
-									</label>
-								</div>
-
-								<div>
-									<label
-										htmlFor="password"
-										className="relative flex flex-col block"
-									>
-										Password
-										<input
-											id="password"
-											placeholder="Enter your password"
-											autoComplete="current-password"
-											name="password"
-											type="password"
-											className="text-black h-12 block bg-transparent w-full px-4 py-2 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-										/>
-										<button className="absolute bottom-0 right-4" type="button">
-											<Image
-												className="h-auto w-auto"
-												src={isEyeOpen ? eyeIcon : eyeHideIcon}
-												alt="Eye icon"
-												width={30}
-												height={30}
-												onClick={handleClick}
-											/>
-										</button>
-									</label>
-								</div>
-
-								<div>
-									<label
-										htmlFor="repeatpassword"
-										className="relative flex flex-col block"
-									>
-										Repeat Password
-										<input
-											placeholder="Re-enter your password"
-											autoComplete="current-password"
-											name="repeatpassword"
-											type="password"
-											className="text-black h-12 bg-transparent block w-full px-4 py-2 mt-2 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-										/>
-									</label>
-								</div>
-								<button
-									className="mx-auto py-2 px-8 mx-auto border border-transparent rounded-md fade-out bg-indigo-600 hover:bg-indigo-900 text-white"
-									type="submit"
-								>
-									Sign Up
-								</button>
-
-								<p className="text-center text-white text-lg">
-									Already have an account?
-									<Link
-										className="hover:text-indigo-600 underline ml-2"
-										href="/signin"
-									>
-										Sign in.
-									</Link>
-								</p>
-							</form>
+								First name
+							</label>
+							<input
+								type="text"
+								name="firstname"
+								placeholder="Enter your first name"
+								className="h-12 text-lg bg-transparent block w-full px-4 py-7 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+							></input>
 						</div>
-					</div>
+
+						<div>
+							<label
+								htmlFor="lastname"
+								className="flex flex-col block text-xl lg:text-2xl"
+							>
+								Last name
+							</label>
+							<input
+								type="text"
+								name="lastname"
+								placeholder="Enter your last name"
+								className="h-12 text-lg py-7 text-black bg-transparent block w-full px-4 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+							></input>
+						</div>
+
+						<div>
+							<label htmlFor="email" className="block">
+								Email
+								<input
+									placeholder="ex: john@gmail.com"
+									className="text-black text-lg py-7 h-12 bg-transparent block w-full px-4 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+									autoComplete="email"
+									name="email"
+									type="email"
+								/>
+							</label>
+						</div>
+
+						<div>
+							<label
+								htmlFor="password"
+								className="relative flex flex-col block text-xl lg:text-2xl"
+							>
+								Password
+								<input
+									id="password"
+									placeholder="Enter your password"
+									autoComplete="current-password"
+									name="password"
+									type="password"
+									className="text-black text-lg py-7 h-12 block bg-transparent w-full px-4 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+								/>
+								<button className="absolute bottom-0 right-4" type="button">
+									<Image
+										className="h-auto w-auto"
+										src={isEyeOpen ? eyeIcon : eyeHideIcon}
+										alt="Eye icon"
+										width={30}
+										height={30}
+										onClick={handleClick}
+									/>
+								</button>
+							</label>
+						</div>
+
+						<div>
+							<label
+								htmlFor="repeatpassword"
+								className="relative flex flex-col block text-xl lg:text-2xl"
+							>
+								Repeat Password
+								<input
+									placeholder="Re-enter your password"
+									autoComplete="current-password"
+									name="repeatpassword"
+									type="password"
+									className="text-black text-lg h-12 bg-transparent block w-full px-4 py-7 mt-2 bg-white border border-gray-200 rounded-md focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+								/>
+							</label>
+						</div>
+						<button
+							className="my-4 mx-auto text-xl py-3 px-8 mx-auto border border-transparent rounded-md fade-out bg-indigo-600 hover:bg-indigo-900 text-white"
+							type="submit"
+						>
+							Sign Up
+						</button>
+
+						<p className="text-center text-white text-lg">
+							Already have an account?
+							<Link
+								className="text-indigo-400 hover:text-white hover:underline ml-2"
+								href="/signin"
+							>
+								Sign in.
+							</Link>
+						</p>
+					</form>
 				</div>
 			</div>
 		</section>
