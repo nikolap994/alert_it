118ac37084c2fd8f1ab382f23a0434091395808a
Marija Ćehić <cehicmarija@gmail.com>
2023-01-23 19:27:18 +0100

    Update Search Monitor styles

--- pages/status/index.js
+++ pages/status/index.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 function SearchMonitor(props) {
 	const router = useRouter();
 
-	const handleSubmit = async event => {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
 
 		const searchId = event.target.searchInput.value;
@@ -28,7 +28,7 @@ function SearchMonitor(props) {
 						Search Monitors by ID
 					</h2>
 					<input
-						className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 max-w-[1000px]"
+						className="block p-4 pl-10 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 max-w-[1000px]"
 						placeholder="Monitor ID"
 						type="text"
 						name="searchInput"
@@ -36,7 +36,7 @@ function SearchMonitor(props) {
 					/>
 					<button
 						type="submit"
-						className="text-white absolute right-2.5 bottom-2.5  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-8 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 					>
 						Search
 					</button>
